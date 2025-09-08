42f027908b50a05b319d3453302fd88367faa392
Faizan <faizanali3n@gmail.com>
2025-09-08 15:55:37 +0500

    Error removed

--- src/Components/TicTacToe.jsx
+++ src/Components/TicTacToe.jsx
@@ -8,6 +8,21 @@ import cross_icon from './Assets/cross.png'
     <div className ='container'>
     <h1 className='title'>Tic Tac Toe Game In <span>React</span></h1>
     <div className="Board">
+      <div className='row1'>
+      <div className='boxes'></div>
+        <div className='boxes'></div>
+          <div className='boxes'></div>
+    </div>
+      <div className='row2'>
+      <div className='boxes'></div>
+        <div className='boxes'></div>
+          <div className='boxes'></div>
+    </div>
+      <div className='row3'>
+      <div className='boxes'></div>
+        <div className='boxes'></div>
+          <div className='boxes'></div>
+    </div>
     </div>
   <button className="reset">Reset</button>
 
